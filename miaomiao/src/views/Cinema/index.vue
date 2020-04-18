88f3ca548a6379dc26e58fbbd8c58edae4f00bc5
yzx <1142069238@qq.com>
2020-04-18 23:14:11 +0800

    2

---
+++ miaomiao/src/views/Cinema/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="main">
+    <Head title="喵喵影院"/>
+    <div id="content">
+    <div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+       
+			</div>
+       <Cilist/>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Head from "@/components/head";
+import Footer from "@/components/TabBar";
+import Cilist from "@/components/Cilist";
+export default {
+    name:"Cinema",
+  components: {
+    Head,
+    Footer,
+    Cilist
+  }
+};
+</script>
+<style scoped>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
---
+++ miaomiao/src/views/Mine/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="main">
+    <Head title="登录喵喵"/>
+    <div id="content">
+      <Login/>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Head from "@/components/head";
+import Footer from "@/components/TabBar";
+import Login from "@/components/Login";
+export default {
+    name:"mine",
+  components: {
+    Head,
+    Footer,
+    Login
+  }
+};
+</script>
+<style scoped>
+</style>
